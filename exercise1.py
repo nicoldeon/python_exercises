4527eab0c704700cbefa5d1712d204c813edff78
Nicole <nicole.on.goldenowl@gmail.com>
2023-04-04 17:00:02 +0700

    update

--- exercise1.py
+++ exercise1.py
@@ -1,19 +1,30 @@
-def find_max_in_list(ls):
-    # read list from file
+def read_from_file(ls):
     with open('exercise1.txt', 'r') as file:
         for line in file:
             for num in line.split():
                 ls.append(int(num))
 
-    max_ls = 0
+
+def find_max_in_list(ls):
+    # if length of list is 0, return -1, dont exits max value
+    if (len(ls) == 0):
+        return -1
+
+    # if length of list is 1, max element is the only element of the list
+    if len(ls) == 1:
+        return ls[0]
+
+    max_ls = ls[0]
 
     # find max in list
     for x in ls:
         if x > max_ls:
             max_ls = x
+
     return max_ls
 
 
 if __name__ == "__main__":
-    lis = []
-    print(find_max_in_list(lis))
+    ls = []
+    read_from_file(ls)
+    print(find_max_in_list(ls))
--- exercise2.py
+++ exercise2.py
@@ -1,21 +1,51 @@
-def find_second_max_list(ls):
-    # read list from file
+def read_from_file(ls):
     with open('exercise2.txt', 'r') as file:
         for line in file:
             for num in line.split():
                 ls.append(int(num))
 
-    n = len(ls)
-    # sort list descending
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if ls[j] < ls[j+1]:
-                ls[j], ls[j+1] = ls[j+1], ls[j]
 
-    # return the second element (second max) of list descending
-    return ls[1]
+def find_max_in_list(ls):
+    # if length of list is 0, dont exist max value
+    if (len(ls) == 0):
+        return -1
+
+    # if length of list is 1, max element is the only element of the list
+    if len(ls) == 1:
+        return ls[0]
+
+    max_ls = ls[0]
+
+    # find max in list
+    for x in ls:
+        if x > max_ls:
+            max_ls = x
+
+    return max_ls
+
+
+def find_second_max_list(ls):
+    # if length of list is 0, dont exist second max element
+    if (len(ls) == 0):
+        return -1
+
+    # if length of list is 1, dont exist second max element
+    if (len(ls) == 1):
+        return -1
+
+    # find max in list
+    max_ls = find_max_in_list(ls)
+    second_max = ls[0]
+
+    # find second max in list
+    for x in ls:
+        if x > second_max and x < max_ls:
+            second_max = x
+
+    return second_max
 
 
 if __name__ == "__main__":
     ls = []
+    read_from_file(ls)
     print(find_second_max_list(ls))
