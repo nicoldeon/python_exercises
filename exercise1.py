ff0b5a7d0e32eb3f54ace504fb01d5168db5d5ec
Nicole <nicole.on.goldenowl@gmail.com>
2023-04-04 15:52:43 +0700

    update file

--- exercise1.py
+++ exercise1.py
@@ -1,32 +1,22 @@
 import random
 
 
-def input_list(lis):
-    n = int(input("Input number of elements of list:"))
-    if type(n) == int:
-        for i in range(n):
-            if type(i) == int:
-                ele = int(input(f"Input element {i+1}: "))
-                lis.append(ele)
-            else:
-                print("The input type is not integer, please try again")
-    else:
-        print("The input type is not integer, please try again")
-    return lis
-
-
 def find_max_in_list(lis):
+    # read list from file
+    with open('exercise1.txt', 'r') as file:
+        for line in file:
+            for num in line.split():
+                ls.append(int(num))
+
     max_ls = 0
+
+    # find max in list
     for x in lis:
-        if type(x) == int:
-            if x > max_ls:
-                max_ls = x
-        else:
-            continue
+        if x > max_ls:
+            max_ls = x
     return max_ls
 
 
 if __name__ == "__main__":
     ls = []
-    input_list(ls)
     print(find_max_in_list(ls))
--- exercise2.py
+++ exercise2.py
@@ -1,48 +1,24 @@
 import random
 
 
-def input_list(lis):
-    n = int(input("Input number of elements of list:"))
-    if type(n) == int:
-        for i in range(n):
-            if type(i) == int:
-                ele = int(input(f"Input element {i+1}: "))
-                lis.append(ele)
-            else:
-                print("The input type is not integer, please try again")
-    else:
-        print("The input type is not integer, please try again")
-    return lis
-
-
-def find_max_in_list(lis):
-    max_ls = 0
-    for x in lis:
-        if type(x) == int:
-            if x > max_ls:
-                max_ls = x
-        else:
-            continue
-    return max_ls
-
-
 def find_second_max_list(lis):
-    second_max = 0
-    max_lis = find_max_in_list(lis)
-    for x in lis:
-        if type(x) == int:
-            if x == max_lis:
-                continue
-            else:
-                if x > second_max:
-                    second_max = x
-        else:
-            continue
-    return second_max
+    # read list from file
+    with open('exercise2.txt', 'r') as file:
+        for line in file:
+            for num in line.split():
+                ls.append(int(num))
+
+    n = len(ls)
+    # sort list descending
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if ls[j] < ls[j+1]:
+                ls[j], ls[j+1] = ls[j+1], ls[j]
+
+    # return the second element (second max) of list descending
+    return ls[1]
 
 
 if __name__ == "__main__":
     ls = []
-    input_list(ls)
-    print(find_max_in_list(ls))
     print(find_second_max_list(ls))
