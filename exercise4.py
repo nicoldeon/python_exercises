8d3ca8d24bf560918ac47252c4915c6b63eb69fa
Nicole <nicole.on.goldenowl@gmail.com>
2023-04-06 09:25:07 +0700

    update exercise4

--- exercise4.py
+++ exercise4.py
@@ -9,61 +9,71 @@ class CalculationAngle:
         return self.hour
 
     def set_hour(self):
-        hour = int(input("Input hour: "))
-        if hour != "":
-            if type(hour) == int:
-                if 0 <= hour <= 24:
+        try:
+            hour = input("Input hour: ")
+            if hour:
+                hour = int(hour)
+                if 0 <= hour <= 60:
                     self.hour = hour
                 else:
-                    print("Your hour is out of range!")
+                    print("Your minute if out of range!")
             else:
-                print("Please input valid hour!")
-        else:
-            print("Can not let hour empty!")
+                print("You need to input some value!")
+        except ValueError:
+            print("You need to input a number!")
 
     def get_minute(self):
         return self.minute
 
     def set_minute(self):
-        minute = int(input("Input minute: "))
-        if minute != "":
-            if type(minute) == int:
+        try:
+            minute = input("Input minute: ")
+            if minute:
+                minute = int(minute)
                 if 0 <= minute <= 60:
                     self.minute = minute
                 else:
-                    print("Your minute is out of range!")
+                    print("Your minute if out of range!")
             else:
-                print("Please input valid minute!")
-        else:
-            print("Can not let hour empty!")
+                print("You need to input some value!")
+        except ValueError:
+            print("You need to input a number!")
 
-    # calculate angle between hour hand and minute hand
+    # calculate angle between hour hand and minute hands
+    def check_if_int(self, num):
+        if type(num) == int:
+            return True
+        return False
+
     def calculate_angle(self):
         hour = self.get_hour()
         minute = self.get_minute()
 
-        # reference to 12
-        if hour == 12:
-            hour = 0
+        if hour and minute:
+            # reference to 12
+            if hour == 12:
+                hour = 0
 
-        if minute == 60:
-            minute = 0
-            hour += 1
+            if minute == 60:
+                minute = 0
+                hour += 1
 
-        if hour > 12:
-            hour = hour - 12
+            if hour > 12:
+                hour = hour - 12
 
-        # calculate angle of hour hand, 1 hour -> hour hand move 30 degrees, 1 minute -> hour hand move 6/30 = 0.5 degrees
-        angleHour = hour * 30 + minute * 0.5
+            # calculate angle of hour hand, 1 hour -> hour hand move 30 degrees, 1 minute -> hour hand move 6/30 = 0.5 degrees
+            angleHour = hour * 30 + minute * 0.5
 
-        # calculate angle of minute hand, 1 minute -> minute hand move 6 degrees
-        angleMinute = minute * 6
+            # calculate angle of minute hand, 1 minute -> minute hand move 6 degrees
+            angleMinute = minute * 6
 
-        # calculate angle between hour hand and minute hand
-        ans = abs(angleHour - angleMinute)
+            # calculate angle between hour hand and minute hand
+            ans = abs(angleHour - angleMinute)
 
-        # return min between ans and 360 - ans
-        return min(360 - ans, ans)
+            # return min between ans and 360 - ans
+            return min(360 - ans, ans)
+        else:
+            print("Please input a hour value and a minute value!")
 
 
 if __name__ == '__main__':
